7c9561d045abae1bb339b767e086f76259600aee
Linh Nguyen <linh.nguyen@galaxytechnology.vn>
2024-12-26 11:18:54 +0700

    feat: update gitignore file

--- dist/utils/check-uuid.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.checkUuid = void 0;
-const checkUuid = (str) => /^[0-9a-f]{8}-[0-9a-f]{4}-[0-5][0-9a-f]{3}-[089ab][0-9a-f]{3}-[0-9a-f]{12}$/i.test(str);
-exports.checkUuid = checkUuid;
